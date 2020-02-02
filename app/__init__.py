ac92ed20487da41668738308f0efb3632093364e
Trinity John <trinityrtime@gmail.com>
2020-02-03 01:45:39 +0300

    news html file and content

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from .config import DevConfig
+
 # from flask_bootstrap import Bootstrap
 # from config import config_options
 
 #initializing app
 app = Flask(__name__)
+
+# setting up configuration
+app.config,from_object (DevConfig)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,14 +1,22 @@
 from flask import render_template
 from app import app
 
+
 # Views
-@app.route("/movuie/<movie_id>")
+@app.route('/')
 def index():
 
-  '''
-  View root page function returns index page and its data
-  '''
+    '''
+    View root page function returns index page and its data
+    '''
 
-  message = 'NEWS TODAY!'
-  return render_template('index.html', message = message)
-  
+    title = 'Home - WELCOME TO BEST NEWS TODAY REVIEW PAGE ONLINE!'
+    return render_template('index.html', title = title)
+    
+    # @app.route('/news/<int:news_id>')  
+    # def news(news_id):
+
+    #     '''
+    #     View news page function that returns the news details page and its data
+    #     '''
+    #     return render_template('news.html',id = news_id)
