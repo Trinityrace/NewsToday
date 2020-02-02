343b7b7c6eac22bfc569da64d519ba33c0ed8d62
Trinity John <trinityrtime@gmail.com>
2020-02-03 02:00:59 +0300

    details and first test

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,11 @@ from .config import DevConfig
 # from config import config_options
 
 #initializing app
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config = True)
+
 
 # setting up configuration
 app.config,from_object (DevConfig)
+app.config.from_pyfile ('config.py')
 
 from app import views
---
+++ app/requests.py
@@ -0,0 +1,4 @@
+from app import app
+
+# Getting api key
+api_key = app.config['64281a2732dd4fbba93a0ce3e0352cc5']
