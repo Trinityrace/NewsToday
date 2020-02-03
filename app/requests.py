49f9840b9d53ae4c41b91379c9307ad8ef6ab7f1
Trinity John <trinityrtime@gmail.com>
2020-02-03 08:13:49 +0300

    requests file details

--- app/requests.py
+++ app/requests.py
@@ -1,4 +1,32 @@
 from app import app
+import urllib.request,json
+from .models import news
+
+
+News = news.News
 
 # Getting api key
 api_key = app.config['64281a2732dd4fbba93a0ce3e0352cc5']
+
+# Getting the news base url
+base_url = app.config['https://newsapi.org/v2/top-headlines?'
+       'country=us&']
+
+def get_news(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_news_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['results']:
+            news_results_list = get_news_response['results']
+            news_results = process_results(news_results_list)
+
+
+    return news_results
