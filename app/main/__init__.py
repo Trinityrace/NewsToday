f000cc9f74f0801c642eac5d76648470b9a20c46
Trinity John <trinityrtime@gmail.com>
2020-02-05 10:07:23 +0300

    changed news.html to articles.html,;;edits on files in main folder;; edit files in templates folder;; added navbar html

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,15 +1,16 @@
-from flask import Flask
-from .config import DevConfig
-
-# from flask_bootstrap import Bootstrap
-# from config import config_options
-
-#initializing app
-app = Flask(__name__, instance_relative_config = True)
+from flask import Blueprint
+main = Blueprint('main',__name__)
+from . import views, error
 
 
-# setting up configuration
-app.config,from_object (DevConfig)
-app.config.from_pyfile ('config.py')
+# from flask import Flask
+# from .config import DevConfig
+# # from flask_bootstrap import Bootstrap
+# # from config import config_options
+# #initializing app
+# app = Flask(__name__, instance_relative_config = True)
+# # setting up configuration
+# app.config,from_object (DevConfig)
+# app.config.from_pyfile ('config.py')
 
-from app import views
+# from app import views
